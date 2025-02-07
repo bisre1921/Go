36853aae51687a613828ba7fb61611325131aa84
bisre1921 <bisrattewodros3@gmail.com>
2025-02-07 22:19:59 +0300

    setup routes

--- go-and-mysql/pkg/routes/bookStoreRoutes.go
+++ go-and-mysql/pkg/routes/bookStoreRoutes.go
@@ -6,12 +6,12 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// SetBookStoreRoutes sets the routes for the book store
+// SetBookStoreRoutes sets the routes for the book store API
 func SetBookStoreRoutes(router *mux.Router) *mux.Router {
-	router.HandleFunc("/books", controllers.GetBooks).Methods("GET")
-	router.HandleFunc("/books/{id}", controllers.GetBook).Methods("GET")
-	router.HandleFunc("/books", controllers.CreateBook).Methods("POST")
-	router.HandleFunc("/books/{id}", controllers.UpdateBook).Methods("PUT")
-	router.HandleFunc("/books/{id}", controllers.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/api/bookstore", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/api/bookstore", controllers.GetBooks).Methods("GET")
+	router.HandleFunc("/api/bookstore/{id}", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/api/bookstore/{id}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/api/bookstore/{id}", controllers.DeleteBook).Methods("DELETE")
 	return router
 }
