71e717131cf7bc1b0ef1e83adba3d45ec215ad35
bisre1921 <bisrattewodros3@gmail.com>
2025-02-11 19:44:08 +0300

    structs in golang

---
+++ crash-course2/structs/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	name string
+	age  int
+}
+
+func main() {
+	fmt.Println("Structs in Go")
+	// no inheritance in golang
+	// no classes in golang
+	// no constructors in golang
+	// no generics in golang
+
+	// creating a struct
+	p1 := Person{"John", 30}
+	fmt.Println(p1)
+	fmt.Printf("p1 details: %+v\n", p1)
+}
