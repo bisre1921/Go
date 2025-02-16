78ecea9f283a32be0feebc378dc12f870a08fb5c
bisre1921 <bisrattewodros3@gmail.com>
2025-02-16 14:33:20 +0300

    get one course based on id in golang

--- crash-course2/buildApi/main.go
+++ crash-course2/buildApi/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Course struct {
@@ -37,4 +39,19 @@ func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("get all courses")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("get one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	for _, course := range courses {
+		if course.ID == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("Course not found with the given id")
+	return
 }
