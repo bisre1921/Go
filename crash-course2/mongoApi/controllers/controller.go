c67a5a0366b6fe709b5079ce9e08d8faae0da4da
bisre1921 <bisrattewodros3@gmail.com>
2025-02-17 11:35:45 +0300

    make a mongodb database connection

---
+++ crash-course2/mongoApi/controllers/controller.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+var connectionString = "mongodb+srv://bisrat:bisrat1234@mongoandgoapi.amequ.mongodb.net/?retryWrites=true&w=majority&appName=mongoAndGoApi"
+var dbName = "movies"
+var collectionName = "watchlist"
+
+var collection *mongo.Collection
+
+func init() {
+	clientOption := options.Client().ApplyURI(connectionString)
+	client, err := mongo.Connect((clientOption))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+
+	collection = client.Database(dbName).Collection(collectionName)
+	fmt.Println("Collection instance created!")
+
+}
