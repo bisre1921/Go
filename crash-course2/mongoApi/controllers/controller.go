26c1ab0575af096e82cda4022f5794386fcc2bcb
bisre1921 <bisrattewodros3@gmail.com>
2025-02-17 13:50:06 +0300

    update a record in mongodb

--- crash-course2/mongoApi/controllers/controller.go
+++ crash-course2/mongoApi/controllers/controller.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"mongoApi/models"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
@@ -35,4 +37,15 @@ func insertOne(movie models.Movie) {
 		log.Fatal(err)
 	}
 	fmt.Println("Inserted a Single Record ", insertResult.InsertedID)
+}
+
+func updateOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Updated a Single Record ", result.UpsertedID)
 }
